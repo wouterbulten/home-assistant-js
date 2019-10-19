7df95bf1791f148e7763ed0f577198339040d21f
Wouter Bulten <wouterbulten@mai-ai.org>
2019-10-19 16:07:07 +0200

    Update library and examples

--- js/ha.js
+++ js/ha.js
@@ -1,3 +1,14 @@
+/**
+ * Home Assistant API connection class.
+ *
+ * @class
+ * @author Wouter Bulten
+ * @see {@link https://github.com/wouterbulten/home-assistant-js}
+ * @version Version: 0.1.0
+ * @copyright Copyright 2019 Wouter Bulten
+ * @license MIT License
+ * @preserve
+ */
 class HaAPI {
 
   /**
@@ -12,10 +23,11 @@ class HaAPI {
 
   /**
    * Get state of an entity.
-   * @param  {string}  entity entity_id
+   * @param  {string}  entity       Entity id of the entity.
+   * @param  {boolean} [full=false] Whether to return the full state object. Default = false.
    * @return {Promise}
    */
-  async getState(entity) {
+  async getState(entity, full = false) {
     // Create headers for authentication
     const headers = {
       'Content-Type': 'application/json',
@@ -25,7 +37,50 @@ class HaAPI {
     const url = `${this.instance}/api/states/${entity}`;
 
     const response = await fetch(url, { headers });
+    const state = await response.json();
 
-    return await response.json();
+    return full ? state : state['state'];
+  }
+
+  /**
+   * Get attribute of an entity.
+   * @param {string} entity     Entity id of the entity.
+   * @param {string} attribute  Name of the attribute.
+   * @return {Promise}
+   */
+  async getAttribute(entity, attribute) {
+
+    const state = await this.getState(entity, true);
+
+    if(!(attribute in state['attributes'])) {
+      throw `Attribute ${attribute} does not exist for entity ${entity}.`;
+    }
+
+    return state['attributes'][attribute];
+  }
+
+  /**
+   * Set state of an entity.
+   *
+   * Returns the new state on success.
+   * @param  {string} entity Entity id of the entity.
+   * @param  {string} state  New value of the entity.
+   * @return {Promise}
+   */
+  async setState(entity, state) {
+    // Create headers for authentication
+    const headers = {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${this.token}`
+    };
+
+    const url = `${this.instance}/api/states/${entity}`
+    const body = JSON.stringify({ state });
+
+    const response = await fetch(url, { method: 'POST', headers, body });
+    const newState = await response.json();
+
+    return newState['state'];
   }
 }
